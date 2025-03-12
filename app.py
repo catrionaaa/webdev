9a388f3cb3ea26223d648ad4ae8c125f92ee02b2
Liz <lizh7613@gmail.com>
2025-03-12 11:10:26 +0000

    added a page for high low

--- app.py
+++ app.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+@app.route('/highlow')
+def highlow():
+    return render_template('highlow.html')
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8000)

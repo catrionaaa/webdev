d21ebd4c9ef2f59be52a6c80a45430fec4712625
Liz <lizh7613@gmail.com>
2025-03-18 12:18:02 +0000

    progress

--- static/snap.js
+++ static/snap.js
@@ -1,51 +1,67 @@
-const pile1Name = "pile1hW02fZCyu1"
-const pile2Name = "pile2hCs9HGCBLP"
-
-//make a new deck and save the deckID
+//make a new deck, save the deckID to local storage and deal the cards evenly between two piles
 function fetchDeck() {
   const newDeckUrl = "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1"
+  const pile1Name = "1"
+  const pile2Name = "2"
 
   fetch(newDeckUrl)
     .then((res) => res.json())
     .then((data) => {
-      console.log(data);
+      console.log("Received deck")
       localStorage.setItem("deckID", data.deck_id);  
     })
     .catch((err) => {
       console.error(err);
     });
 
-  console.log(localStorage.getItem("deckID"));
-
-  dealCards();
+  dealCards(pile1Name, pile2Name);
 }
 
 //deal the entire deck from fetchDeck() into two piles
-function dealCards() {
+function dealCards(pile1, pile2) {
+  for (let i = 0; i < 26; i++) {
+    dealCard(pile1);
+    dealCard(pile2);
+  }
 
+  const deckID = localStorage.getItem("deckID");
+  
+  fetch("https://deckofcardsapi.com/api/deck/" + deckID + "/pile/" + pile1 + "/list/")
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch((err) => console.error(err));
+
+  fetch("https://deckofcardsapi.com/api/deck/" + deckID + "/pile/" + pile2 + "/list/")
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch((err) => console.error(err));
+  
 }
 
 //draw a single card from the deck and place it in a pile
-function dealCard() {
+function dealCard(pile) {
   const deckID = localStorage.getItem("deckID");
 
-  console.log(deckID);
-
-  fetch("https://deckofcardsapi.com/api/deck/" + deckID + "/draw")
+  fetch("https://deckofcardsapi.com/api/deck/" + deckID + "/draw?count=1")
     .then(res => res.json())
     .then(data => {
-      pileCard(data.cards[0]);
+      pileCard(pile, data.cards[0]);
     })
     .catch((err) => {
       console.error(err);
     })
-  
-  console.log(card);
 }
 
 //place a single card into the pile provided
-function pileCard(card) {
+function pileCard(pile, card) {
+  const deckID = localStorage.getItem("deckID");
 
+  console.log(card.code);
+
+  fetch("https://deckofcardsapi.com/api/deck/" + deckID + "/pile/" + pile + "/add/?cards=" + card.code)
+    .catch((err) => {
+      console.error(err);
+    })
 }
 
 fetchDeck();

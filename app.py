a37e5eac11b392d603f244f4e7c024c3aea9ef5f
catrionaaa <149389234+catrionaaa@users.noreply.github.com>
2025-03-21 10:47:12 +0000

    added db import

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, jsonify
 from flask_restful import Api, Resource
 from flask_sqlalchemy import SQLAlchemy
-from games import Game
+from games import db, Game
 
 app = Flask(__name__)
 

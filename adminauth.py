1211144fedaaa2fd6b6b8e19a0e7644462577223
catrionaaa <149389234+catrionaaa@users.noreply.github.com>
2025-03-22 19:35:08 +0000

    changed key validation to come from db

--- adminauth.py
+++ adminauth.py
@@ -1,14 +1,23 @@
 from functools import wraps
 from flask import request, jsonify
+from models import APIKey
 
 
 def require_api_key(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
+
+        from app import app, db
         api_key = request.headers.get('X-API-KEY')
 
-        if api_key != 'testkey':
-            return {"error": "incorrect api key"}, 403
+        with app.app_context():
+            key_entry = APIKey.query.filter_by(key=api_key).first()
+
+            if not key_entry:
+                return {"error": "incorrect api key"}, 403
+
+            key_entry.request_count += 1
+            db.session.commit()
 
         return f(*args, **kwargs)
 

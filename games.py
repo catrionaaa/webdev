34c81889c57d423d79d103f6007fee5b5cbbff90
catrionaaa <149389234+catrionaaa@users.noreply.github.com>
2025-03-22 19:17:52 +0000

    added apikey model

--- games.py
+++ games.py
@@ -12,3 +12,10 @@ class Key(db.Model):
     owner = db.Column(db.String(50), nullable=False)
     request_count = db.Column(db.Integer, default=0)
     rate_limit = db.Column(db.Integer, default=1000)
+
+class APIKey(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    key = db.Column(db.String(64), unique=True, nullable=False)
+    owner = db.Column(db.String(50), nullable=False)
+    request_count = db.Column(db.Integer, default=0)
+    rate_limit = db.Column(db.Integer, default=1000)

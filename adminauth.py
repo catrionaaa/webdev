23e04e72b2baf0cdf1aa776d4760d45b05943fc9
catrionaaa <149389234+catrionaaa@users.noreply.github.com>
2025-03-22 18:58:29 +0000

    added security with test key

--- adminauth.py
+++ adminauth.py
@@ -1,2 +1,15 @@
 from functools import wraps
-from flask import request
+from flask import request, jsonify
+
+
+def require_api_key(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        api_key = request.headers.get('X-API-KEY')
+
+        if api_key != 'testkey':
+            return {"error": "incorrect api key"}, 403
+
+        return f(*args, **kwargs)
+
+    return decorated_function

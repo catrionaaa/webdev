c8b29f0a38976368de80dc09d3a25c335700a0b0
catrionaaa <149389234+catrionaaa@users.noreply.github.com>
2025-03-24 19:35:02 +0000

    changed app

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 app = Flask(__name__)
 from flask import Flask, render_template, jsonify, request
-from flask_restful import Api
+from flask_restful import Api, Resource
 from flask_sqlalchemy import SQLAlchemy
-from games import db, Game, GameAPI
+from models import db, Game, GameAPI
 from deckOfCards import DeckOfCardsAPI
 from flasgger import Swagger
+from adminauth import require_api_key
 swagger = Swagger(app)
 db.init_app(app)
 api = Api(app)
@@ -82,6 +83,74 @@ def draw_cards(deck_id):
 def shuffle_deck(deck_id):
     shuffle_data = DeckOfCardsAPI.shuffle_deck(deck_id)
     return jsonify(shuffle_data)
+
+# --------------------------
+# Flask endpoints
+# --------------------------
+
+class GameAPI(Resource):
+    @require_api_key
+    def get(self):
+        games = Game.query.all()
+        gameList = []
+
+        for game in games:
+            gameData = {
+                "id": game.id,
+                "type": game.type,
+            }
+            gameList.append(gameData)
+        
+        return jsonify(gameList)
+
+
+    @require_api_key
+    def post(self):
+        data = request.get_json()
+
+        #if not data or "id" not in data or "type" not in data:
+            #return jsonify({"error": "missing data when recording game"}), 400
+
+        newGame = Game(
+            id=data["id"],
+            type=data["type"]
+        )
+
+        db.session.add(newGame)
+        db.session.commit()
+
+        #return jsonify({"message": "game was successfully recorded"}), 201
+
+    @require_api_key
+    def put(self):
+        data = request.json
+        gameId = data.get("id")
+
+        game = Game.query.get(gameId)
+        if not game:
+            return {"error:" "could not find game ID"}
+        
+        game.id = data["id"]
+        game.type = data["type"]
+
+        db.session.commit()
+
+        return {"message": "game successfully updated"}
+    
+
+    @require_api_key
+    def delete(self):
+        data = request.json
+        gameId = data.get("id")
+
+        game = Game.query.get(gameId)
+        if not game:
+            return {"error": "could not find game ID"}
+        
+        db.session.delete(game)
+        db.session.commit()
+
+        return {"message": "game successfully deleted"}
 
 # --------------------------
 # Run the app

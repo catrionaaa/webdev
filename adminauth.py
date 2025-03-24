e0bdb88f316f027ad44dbe4e20bab39c3b512957
catrionaaa <149389234+catrionaaa@users.noreply.github.com>
2025-03-24 19:31:39 +0000

    changed admin

--- adminauth.py
+++ adminauth.py
@@ -1,15 +1,24 @@
-#from functools import wraps
-#from flask import request
-
-from flask import request, jsonify
 from functools import wraps
+from flask import request, jsonify
+from models import APIKey
 
-def admin_required(f):
+
+def require_api_key(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
-        # Check for an admin key in the request headers
-        admin_key = request.headers.get('Admin-Key')
-        if admin_key != "your_secret_admin_key":  # Replace with a secure key
-            return jsonify({"message": "Unauthorized"}), 403
+
+        from app import app, db
+        api_key = request.headers.get('X-API-KEY')
+
+        with app.app_context():
+            key_entry = APIKey.query.filter_by(key=api_key).first()
+
+            if not key_entry:
+                return {"error": "incorrect api key"}, 403
+
+            key_entry.request_count += 1
+            db.session.commit()
+
         return f(*args, **kwargs)
+
     return decorated_function
